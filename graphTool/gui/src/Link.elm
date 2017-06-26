0a76932140d06e5a8b22335b1b2aa2cf09ef6c74
PICARD Michel <michel.picard@sirehna.com>
2017-06-26 17:15:57 +0200

    developpement activation des types de liens

--- graphTool/gui/src/Link.elm
+++ graphTool/gui/src/Link.elm
@@ -2,13 +2,14 @@ module Link
     exposing
         ( Edge
         , ActivePoperties
+        , isActive
         , link
         , makeLink
+        , changeActive
           -- , Link
         )
 
 import Identifier exposing (Identifier)
-import LinkParameters
 import Set exposing (Set)
 
 
@@ -32,3 +33,36 @@ makeLink i s t =
 link : Identifier -> Identifier -> Edge
 link s t =
     makeLink 0 s t
+
+
+changeActive : Identifier -> Edge -> Edge
+changeActive x edge =
+    case isActive x edge of
+        False ->
+            activate x edge
+
+        True ->
+            unActivate x edge
+
+
+isActive : Identifier -> Edge -> Bool
+isActive x edge =
+    Set.member x edge.parameters
+
+
+activate : Identifier -> Edge -> Edge
+activate x edge =
+    let
+        new_parameters =
+            Set.insert x edge.parameters
+    in
+        { edge | parameters = new_parameters }
+
+
+unActivate : Identifier -> Edge -> Edge
+unActivate x edge =
+    let
+        new_parameters =
+            Set.remove x edge.parameters
+    in
+        { edge | parameters = new_parameters }
