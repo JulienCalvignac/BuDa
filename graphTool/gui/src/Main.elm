9fe9e05a84d342a70dd376345d7f2ddb0bcf58f6
PICARD Michel <michel.picard@sirehna.com>
2017-08-21 10:28:11 +0200

    deplacement de code

--- graphTool/gui/src/Main.elm
+++ graphTool/gui/src/Main.elm
@@ -1,64 +1,17 @@
 module Main exposing (..)
 
+import Messages
 import Actions
-import Html exposing (Html, Attribute, button, div, fieldset, input, label, span, text, section)
-import Html.Attributes exposing (id, name, style, type_, checked, value, placeholder, class)
-import Html.Events exposing (onClick, on, onInput)
-import Json.Decode
-import Model exposing (..)
+import Html
+import Model
 import View
-import ParametersView
 
 
-onInputFile : msg -> Html.Attribute msg
-onInputFile message =
-    on "change" (Json.Decode.succeed message)
-
-
-radio : msg -> String -> Html msg
-radio msg value =
-    label
-        [ style [ ( "padding", "5px" ) ]
-        ]
-        [ input [ type_ "radio", name "viewType-radio", onClick msg ] []
-        , text value
-        ]
-
-
-{-| Main application
--}
-view : Model -> Html Actions.Msg
-view model =
-    div []
-        [ button [ onClick Actions.ShowView, id "showView", value "showView" ]
-            [ text "showView" ]
-        , div []
-            [ fieldset [ id "fieldset" ]
-                [ radio (Actions.SwitchToView Model.BULL) "Bubble Diagram"
-                , radio (Actions.SwitchToView Model.PBS) "PBS"
-                , radio (Actions.SwitchToView Model.ALL) "All"
-                ]
-            ]
-        , button [ onClick Actions.CreateNode, id "new", value "new element" ] [ text "new node" ]
-        , button [ onClick Actions.CreateLink, id "edge", value "edge" ] [ text "new link" ]
-        , button [ onClick Actions.RenameNode, id "rename", value "rename" ] [ text "rename" ]
-        , ParametersView.view model
-        , button [ onClick Actions.CreateParameter, id "createParameter", value "createParameter" ] [ text "+" ]
-        , button [ onClick Actions.DeleteParameter, id "deleteParameter", value "deleteParameter" ] [ text "-" ]
-          -- , button [ onClick Actions.ParametersDialog, id "dialog", value "dialog" ] [ text "dialog" ]
-        , button [ onClick Actions.ExportLink, id "export", value "export" ] [ text "export" ]
-        , input [ onInput Actions.InputChange, id "input", placeholder "undefined" ] []
-        , button [ onClick Actions.SaveModel, id "saveModel", value "saveModel" ] [ text "saveModel" ]
-        , input [ onInputFile Actions.LoadModel, id model.loadModelId, type_ "file" ] []
-          -- , input [ id "log", value "" ] [ text "" ]
-        ]
-
-
-main : Program Never Model.Model Actions.Msg
+main : Program Never Model.Model Messages.Msg
 main =
     Html.program
         { init = View.init
-        , view = view
+        , view = View.view
         , update = Actions.update
         , subscriptions = Actions.subscriptions
         }
--- graphTool/gui/src/View.elm
+++ graphTool/gui/src/View.elm
@@ -1,22 +1,60 @@
-module View exposing (init)
+module View exposing (init, view)
 
--- import Decoders
+import Messages
+import Html exposing (Html, Attribute, button, div, fieldset, input, label, span, text, section)
+import Html.Attributes exposing (id, name, style, type_, checked, value, placeholder, class)
+import Html.Events exposing (onClick, on, onInput)
+import Json.Decode
+import Model exposing (..)
+import ParametersView
+import AttributView
 
-import Actions
-import Model
 
-
-init : ( Model.Model, Cmd Actions.Msg )
+init : ( Model.Model, Cmd Messages.Msg )
 init =
-    let
-        m1 =
-            Model.defaultModel
+    ( Model.defaultModel, Cmd.none )
 
-        -- model =
-        --     Decoders.decodePBS Decoders.defaultPBS Model.defaultModel
-        --
-        -- m1 =
-        --     Debug.log "model init:"
-        --         (Decoders.decodeLNK Decoders.defaultLNK model)
-    in
-    ( m1, Cmd.none )
+
+onInputFile : msg -> Html.Attribute msg
+onInputFile message =
+    on "change" (Json.Decode.succeed message)
+
+
+radio : msg -> String -> Html msg
+radio msg value =
+    label
+        [ style [ ( "padding", "5px" ) ]
+        ]
+        [ input [ type_ "radio", name "viewType-radio", onClick msg ] []
+        , text value
+        ]
+
+
+{-| Main application
+-}
+view : Model -> Html Messages.Msg
+view model =
+    div []
+        [ button [ onClick Messages.ShowView, id "showView", value "showView" ]
+            [ text "showView" ]
+        , div []
+            [ fieldset [ id "fieldset" ]
+                [ radio (Messages.SwitchToView Model.BULL) "Bubble Diagram"
+                , radio (Messages.SwitchToView Model.PBS) "PBS"
+                , radio (Messages.SwitchToView Model.ALL) "All"
+                ]
+            ]
+        , button [ onClick Messages.CreateNode, id "new", value "new element" ] [ text "new node" ]
+        , button [ onClick Messages.CreateLink, id "edge", value "edge" ] [ text "new link" ]
+        , button [ onClick Messages.RenameNode, id "rename", value "rename" ] [ text "rename" ]
+        , ParametersView.view model
+        , AttributView.view model
+        , button [ onClick Messages.CreateParameter, id "createParameter", value "createParameter" ] [ text "+" ]
+        , button [ onClick Messages.DeleteParameter, id "deleteParameter", value "deleteParameter" ] [ text "-" ]
+          -- , button [ onClick Actions.ParametersDialog, id "dialog", value "dialog" ] [ text "dialog" ]
+        , button [ onClick Messages.ExportLink, id "export", value "export" ] [ text "export" ]
+        , input [ onInput Messages.InputChange, id "input", placeholder "undefined" ] []
+        , button [ onClick Messages.SaveModel, id "saveModel", value "saveModel" ] [ text "saveModel" ]
+        , input [ onInputFile Messages.LoadModel, id model.loadModelId, type_ "file" ] []
+          -- , input [ id "log", value "" ] [ text "" ]
+        ]
