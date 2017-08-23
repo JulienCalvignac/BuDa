e312a43ba95837eb183792ede4f205ea233d07ec
PICARD Michel <michel.picard@sirehna.com>
2017-08-23 14:55:18 +0200

    modification libelles des boutons...

--- graphTool/gui/src/View.elm
+++ graphTool/gui/src/View.elm
@@ -5,7 +5,7 @@ import Html exposing (Html, Attribute, button, div, fieldset, input, label, span
 import Html.Attributes exposing (id, name, style, type_, checked, value, placeholder, class)
 import Html.Events exposing (onClick, on, onInput)
 import Json.Decode
-import Model exposing (..)
+import Model exposing (ViewType(..))
 import ParametersView
 import AttributView
 
@@ -32,29 +32,30 @@ radio msg value =
 
 {-| Main application
 -}
-view : Model -> Html Messages.Msg
+view : Model.Model -> Html Messages.Msg
 view model =
     div []
-        [ button [ onClick Messages.ShowView, id "showView", value "showView" ]
-            [ text "showView" ]
+        [ button [ onClick Messages.ShowView, id "showView", value "View" ]
+            [ text "View" ]
         , div []
             [ fieldset [ id "fieldset" ]
                 [ radio (Messages.SwitchToView Model.BULL) "Bubble Diagram"
                 , radio (Messages.SwitchToView Model.PBS) "PBS"
                 , radio (Messages.SwitchToView Model.ALL) "All"
+                , radio (Messages.SwitchToView Model.ALL_LIGHT) "Light"
                 ]
             ]
-        , button [ onClick Messages.CreateNode, id "new", value "new element" ] [ text "new node" ]
-        , button [ onClick Messages.CreateLink, id "edge", value "edge" ] [ text "new link" ]
-        , button [ onClick Messages.RenameNode, id "rename", value "rename" ] [ text "rename" ]
+        , button [ onClick Messages.CreateNode, id "new", value "new element" ] [ text "Node" ]
+        , button [ onClick Messages.CreateLink, id "edge", value "edge" ] [ text "Link" ]
+        , button [ onClick Messages.RenameNode, id "rename", value "rename" ] [ text "Name" ]
         , ParametersView.view model
         , AttributView.view model
         , button [ onClick Messages.CreateParameter, id "createParameter", value "createParameter" ] [ text "+" ]
         , button [ onClick Messages.DeleteParameter, id "deleteParameter", value "deleteParameter" ] [ text "-" ]
           -- , button [ onClick Actions.ParametersDialog, id "dialog", value "dialog" ] [ text "dialog" ]
-        , button [ onClick Messages.ExportLink, id "export", value "export" ] [ text "export" ]
+        , button [ onClick Messages.ExportLink, id "export", value "export" ] [ text "Export" ]
         , input [ onInput Messages.InputChange, id "input", placeholder "undefined" ] []
-        , button [ onClick Messages.SaveModel, id "saveModel", value "saveModel" ] [ text "saveModel" ]
+        , button [ onClick Messages.SaveModel, id "saveModel", value "saveModel" ] [ text "Save" ]
         , input [ onInputFile Messages.LoadModel, id model.loadModelId, type_ "file" ] []
           -- , input [ id "log", value "" ] [ text "" ]
         ]
