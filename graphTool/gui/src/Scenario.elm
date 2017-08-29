8f094363f07879ebbc0f2f1c8a28baa1740b3ebb
PICARD Michel <michel.picard@sirehna.com>
2017-08-29 14:11:00 +0200

    definition Messages, et Model pour le Scenario.On considere le undo comme un rejeu de scenario jusqu'a l'ordre n-1

---
+++ graphTool/gui/src/Scenario.elm
@@ -0,0 +1,33 @@
+module Scenario exposing (Msg(..), Model, defaultModel, addMsg)
+
+import Identifier exposing (Identifier)
+import Link exposing (Edge)
+import DataModel
+
+
+type Msg
+    = NoOp
+    | CreateNode String (Maybe Identifier)
+    | CreateLink Identifier Identifier
+    | DeleteLink Identifier
+    | DeleteNode Identifier
+    | RenameNode String (Maybe Identifier)
+    | CreateParameter String
+    | DeleteParameter String
+    | UpdateProperty Edge String
+    | UpdateAttribute String (Maybe Identifier)
+    | LoadModel DataModel.DataModel
+
+
+type alias Model =
+    List Msg
+
+
+defaultModel : Model
+defaultModel =
+    []
+
+
+addMsg : Msg -> Model -> Model
+addMsg msg model =
+    msg :: model
