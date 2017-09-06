990eb269b1309248cc68e8a758177b9e52ef591f
PICARD Michel <michel.picard@sirehna.com>
2017-09-06 17:29:52 +0200

    ajout element group du type Maybe String

--- graphTool/gui/src/Node.elm
+++ graphTool/gui/src/Node.elm
@@ -1,13 +1,23 @@
-module Node exposing (Node, node)
+module Node exposing (Node, node, inGroup)
 
 import Identifier exposing (Identifier)
 import Attribut exposing (Attribut)
 
 
 type alias Node =
-    { id : Identifier, name : String, parent : Maybe Identifier, attribut : Maybe Attribut }
+    { id : Identifier, name : String, parent : Maybe Identifier, attribut : Maybe Attribut, group : Maybe String }
 
 
 node : Identifier -> String -> Maybe Identifier -> Node
 node i s p =
-    { id = i, name = s, parent = p, attribut = Nothing }
+    { id = i, name = s, parent = p, attribut = Nothing, group = Nothing }
+
+
+inGroup : String -> Node -> Bool
+inGroup s n =
+    case n.group of
+        Nothing ->
+            False
+
+        Just g ->
+            (g == s)
