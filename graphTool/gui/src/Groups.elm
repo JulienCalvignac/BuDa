16e007a7a1f286d3aa3e8c33d3ed84b060495ff7
PICARD Michel <michel.picard@sirehna.com>
2017-09-06 17:28:39 +0200

    ajout groups: definition du modele

---
+++ graphTool/gui/src/Groups.elm
@@ -0,0 +1,53 @@
+module Groups exposing (Model, Property, defaultModel, getPropertyIdFromName, property, getPropertyStringFromId)
+
+import Identifier exposing (Identifier)
+
+
+type alias Property =
+    { id : Identifier
+    , name : String
+    }
+
+
+type alias Model =
+    List Property
+
+
+defaultModel : Model
+defaultModel =
+    []
+
+
+property : Identifier -> String -> Property
+property i s =
+    { id = i, name = s }
+
+
+getPropertyStringFromId : Identifier -> Model -> Maybe String
+getPropertyStringFromId id list =
+    case list of
+        [] ->
+            Nothing
+
+        x :: xs ->
+            case x.id == id of
+                True ->
+                    (Just x.name)
+
+                False ->
+                    Nothing
+
+
+getPropertyIdFromName : String -> Model -> Maybe Identifier
+getPropertyIdFromName s list =
+    case list of
+        [] ->
+            Nothing
+
+        x :: xs ->
+            case x.name == s of
+                True ->
+                    (Just x.id)
+
+                False ->
+                    getPropertyIdFromName s xs
