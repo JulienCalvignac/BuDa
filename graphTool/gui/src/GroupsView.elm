33580e14b27c8f0a011ae47fed28a7d91673ebd7
PICARD Michel <michel.picard@sirehna.com>
2017-09-06 18:07:38 +0200

    vue des groupes

---
+++ graphTool/gui/src/GroupsView.elm
@@ -0,0 +1,99 @@
+module GroupsView exposing (view)
+
+import Html exposing (Html, Attribute, button, div, fieldset, input, label, span, text, section)
+import Html.Attributes exposing (id, name, style, type_, checked, value, placeholder, class)
+import Html.Events exposing (onClick, onCheck, on, onInput)
+import Model exposing (Model)
+import Messages
+import DataModel
+import Node exposing (Node)
+
+
+view : Model -> Html Messages.Msg
+view model =
+    div [ id "groups", style [ ( "text-align", "center" ) ] ]
+        [ text "Groups"
+        , Html.hr [] []
+        , expose model
+        ]
+
+
+expose : Model -> Html Messages.Msg
+expose model =
+    let
+        m_id =
+            case model.selection of
+                x :: xs ->
+                    Just x
+
+                [] ->
+                    Nothing
+
+        m_node =
+            case m_id of
+                Nothing ->
+                    Nothing
+
+                Just x ->
+                    DataModel.getNodeFromId x model.dataModel.nodes
+    in
+        exposeList_ m_node (makeKeyValueList m_node model)
+
+
+checkbox : Bool -> msg -> String -> Html msg
+checkbox b msg name =
+    div
+        [ id "checkbox"
+        , style
+            [ ( "padding", "10px" )
+            , ( "text-align", "left" )
+            ]
+        ]
+        [ label []
+            [ input
+                [ type_ "checkbox"
+                , onClick msg
+                , checked b
+                ]
+                []
+            , text name
+            ]
+        ]
+
+
+fluxLine_ : Node -> ( String, Bool ) -> Html Messages.Msg
+fluxLine_ n ( key, b ) =
+    div []
+        [ checkbox b
+            (Messages.CheckNodeGroupProperty n key)
+            -- Messages.NoOp
+            key
+        ]
+
+
+exposeList_ : Maybe Node -> List ( String, Bool ) -> Html Messages.Msg
+exposeList_ m_node list =
+    case m_node of
+        Nothing ->
+            div [] []
+
+        Just n ->
+            case list of
+                [] ->
+                    div [] []
+
+                x :: xs ->
+                    div []
+                        [ fluxLine_ n x
+                        , exposeList_ m_node xs
+                        ]
+
+
+makeKeyValueList : Maybe Node -> Model -> List ( String, Bool )
+makeKeyValueList m_node model =
+    case m_node of
+        Nothing ->
+            List.map (\x -> ( x.name, False )) model.dataModel.groups
+
+        Just node ->
+            List.map (\x -> ( x.name, Node.inGroup x.name node )) model.dataModel.groups
