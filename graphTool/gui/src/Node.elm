0afb57c2f19cfea645bae7aa7201cc7eb67e2a6a
PICARD Michel <michel.picard@sirehna.com>
2017-09-07 17:14:37 +0200

    group: mise a jour du type (Maybe String -> Set Identifier)

--- graphTool/gui/src/Node.elm
+++ graphTool/gui/src/Node.elm
@@ -2,22 +2,28 @@ module Node exposing (Node, node, inGroup)
 
 import Identifier exposing (Identifier)
 import Attribut exposing (Attribut)
+import Set exposing (Set)
 
 
 type alias Node =
-    { id : Identifier, name : String, parent : Maybe Identifier, attribut : Maybe Attribut, group : Maybe String }
+    { id : Identifier
+    , name : String
+    , parent : Maybe Identifier
+    , attribut : Maybe Attribut
+    , group : Set Identifier
+    }
 
 
 node : Identifier -> String -> Maybe Identifier -> Node
 node i s p =
-    { id = i, name = s, parent = p, attribut = Nothing, group = Nothing }
+    { id = i
+    , name = s
+    , parent = p
+    , attribut = Nothing
+    , group = Set.empty
+    }
 
 
-inGroup : String -> Node -> Bool
+inGroup : Identifier -> Node -> Bool
 inGroup s n =
-    case n.group of
-        Nothing ->
-            False
-
-        Just g ->
-            (g == s)
+    Set.member s n.group
