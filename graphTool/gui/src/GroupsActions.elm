24b276caf4c58ca69b16cf2eeb37f0f3a2f75253
PICARD Michel <michel.picard@sirehna.com>
2017-09-13 10:53:16 +0200

    ajout fichier pour les actions sur les groupes

---
+++ graphTool/gui/src/GroupsActions.elm
@@ -0,0 +1,113 @@
+module GroupsActions exposing (addGroupToNode, deleteGroupFromNode)
+
+import Identifier exposing (Identifier)
+import Node exposing (Node)
+import DataModel exposing (Model, childs)
+import ModelManagement
+import Set
+
+
+{--
+////////////////////////////////////////////////////////////////////////////////
+addNodeToGroup
+////////////////////////////////////////////////////////////////////////////////
+--}
+
+
+addGroupToNode : Identifier -> Node -> Model -> Model
+addGroupToNode id n model =
+    let
+        asc_n =
+            ModelManagement.getAscendants model.nodes n Nothing
+    in
+        addGroupToListNode_ id asc_n model
+
+
+addGroupToListNode_ : Identifier -> List Node -> Model -> Model
+addGroupToListNode_ id list model =
+    case list of
+        [] ->
+            model
+
+        x :: xs ->
+            addGroupToListNode_ id xs (addGroupToOneNode_ id x model)
+
+
+addGroupToOneNode_ : Identifier -> Node -> Model -> Model
+addGroupToOneNode_ id n model =
+    let
+        newNodes =
+            List.map
+                (\x ->
+                    case x.id == n.id of
+                        True ->
+                            { x | group = Set.insert id x.group }
+
+                        False ->
+                            x
+                )
+                model.nodes
+    in
+        { model | nodes = newNodes }
+
+
+
+{--
+////////////////////////////////////////////////////////////////////////////////
+deleteGroupFromNode
+////////////////////////////////////////////////////////////////////////////////
+--}
+
+
+deleteGroupFromNode : Identifier -> Node -> Model -> Model
+deleteGroupFromNode id n model =
+    let
+        asc_n =
+            ModelManagement.getAscendants model.nodes n Nothing
+    in
+        deleteGroupFromListNode_ id asc_n model
+
+
+deleteGroupFromListNode_ : Identifier -> List Node -> Model -> Model
+deleteGroupFromListNode_ id list model =
+    case list of
+        [] ->
+            model
+
+        x :: xs ->
+            deleteGroupFromListNode_ id xs (deleteGroupFromOneNode_ id x model)
+
+
+deleteGroupFromOneNode_ : Identifier -> Node -> Model -> Model
+deleteGroupFromOneNode_ id n model =
+    case (canDeleteGroupFromNode id n model) of
+        False ->
+            model
+
+        True ->
+            let
+                newNodes =
+                    List.map
+                        (\x ->
+                            case x.id == n.id of
+                                True ->
+                                    { x | group = Set.remove id x.group }
+
+                                False ->
+                                    x
+                        )
+                        model.nodes
+            in
+                { model | nodes = newNodes }
+
+
+canDeleteGroupFromNode : Identifier -> Node -> Model -> Bool
+canDeleteGroupFromNode id n model =
+    let
+        childs_ =
+            DataModel.childs n model.nodes
+
+        l =
+            List.filter (\x -> Set.member id x.group) childs_
+    in
+        (List.length l == 0)
