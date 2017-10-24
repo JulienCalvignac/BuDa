8bd9bb77790691867afe66906f934bca8525f19a
PICARD Michel <michel.picard@sirehna.com>
2017-10-24 16:59:54 +0200

    deplacement code vers PlayerDataModel.elm

--- graphTool/gui/src/Player.elm
+++ graphTool/gui/src/Player.elm
@@ -5,64 +5,11 @@ import Scenario exposing (Msg(..))
 import DataModelActions
 
 
-playOne : Msg -> DataModel.Model -> DataModel.Model
-playOne msg model =
-    let
-        newModel =
-            case msg of
-                CreateNode s m_id ->
-                    DataModelActions.createNode s m_id model
 
-                CreateLink ids idt ->
-                    DataModelActions.createLink ids idt model
 
-                DeleteLink id ->
-                    DataModelActions.deleteEdge id model
 
-                DeleteNode id ->
-                    DataModelActions.deleteNode id model
 
-                RenameNode s m_id ->
-                    DataModelActions.renameNode s m_id model
 
-                CreateParameter s ->
-                    DataModelActions.createParameter s model
-
-                DeleteParameter s ->
-                    DataModelActions.deleteParameter s model
-
-                UpdateProperty edge s ->
-                    DataModelActions.updateProperty edge s model
-
-                UpdateAttribute s m_id ->
-                    DataModelActions.updateAttribute m_id s model
-
-                LoadModel elements ->
-                    DataModel.dataModelToModel elements model
-
-                GroupNodes list s ->
-                    DataModelActions.groupNodes list s model
-
-                CreateGroup s ->
-                    DataModelActions.createGroup s model
-
-                DeleteGroup s ->
-                    DataModelActions.deleteGroup s model
-
-                UpdateNodeGroupProperty n s ->
-                    DataModelActions.updateNodeGroupProperty n s model
-
-                UpdateTightnessForGroup s edgeId ->
-                    DataModelActions.updateTightnessForGroup s edgeId model
-
-                UpdateLayoutFromNodeId m_id list ->
-                    DataModelActions.updateLayoutFromNodeId m_id list model
-
-                UpdateLightLayout layout ->
-                    DataModelActions.updateLightLayout layout model
-
-                _ ->
-                    model
     in
         newModel
 
