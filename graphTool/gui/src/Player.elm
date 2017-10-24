c0e5b3304181ac03a29aa95aafaef0296bd42ef6
PICARD Michel <michel.picard@sirehna.com>
2017-10-24 17:02:53 +0200

    adaptation du code. distinction des cas CtrlX,CtrlC,CtrlV(necessite tmpDataModel)  des autres cas(DataModel suffit)

--- graphTool/gui/src/Player.elm
+++ graphTool/gui/src/Player.elm
@@ -1,20 +1,60 @@
-module Player exposing (play, redo)
+module Player exposing (play, redo, doCtrlX, doCtrlV, doCtrlC)
 
-import DataModel
 import Scenario exposing (Msg(..))
+import Model
+import PlayerDataModel
+import Identifier exposing (Identifier)
 import DataModelActions
+import DataModel
+import ModelManagement
 
 
+isDataMsg : Msg -> Bool
+isDataMsg msg =
+    case msg of
+        CtrlC _ ->
+            True
+
+        CtrlV _ ->
+            True
+
+        CtrlX _ ->
+            True
+
+        _ ->
+            False
 
 
+playOne : Msg -> Model.Model -> Model.Model
+playOne msg model =
+    let
+        newModel =
+            case (isDataMsg msg) of
+                False ->
+                    let
+                        newDataModel =
+                            PlayerDataModel.playOne msg model.dataModel
+                    in
+                        { model | dataModel = newDataModel }
 
+                True ->
+                    case msg of
+                        CtrlC id ->
+                            doCtrlC id model
 
+                        CtrlX id ->
+                            doCtrlX id model
 
+                        CtrlV m_id ->
+                            doCtrlV m_id model
+
+                        _ ->
+                            model
     in
         newModel
 
 
-play : Scenario.Model -> DataModel.Model -> DataModel.Model
+play : Scenario.Model -> Model.Model -> Model.Model
 play list model =
     case list of
         x :: xs ->
@@ -24,7 +64,7 @@ play list model =
             model
 
 
-redo : Scenario.RedoModel -> DataModel.Model -> DataModel.Model
+redo : Scenario.RedoModel -> Model.Model -> Model.Model
 redo m_redo model =
     case m_redo of
         Just x ->
