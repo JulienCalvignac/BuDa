984538c5b2b0680fca01439c2cd11c3691a0d493
PICARD Michel <michel.picard@sirehna.com>
2018-01-18 18:35:21 +0100

    implementation de fonction verification de unicité des blocs

--- graphTool/gui/src/Verification.elm
+++ graphTool/gui/src/Verification.elm
@@ -1,5 +1,6 @@
-module Verification exposing (verification)
+module Verification exposing (verification, verificationBlocs)
 
+import Identifier exposing (Identifier)
 import DataModel exposing (Model)
 import Link exposing (Edge)
 import ModelManagement
@@ -8,6 +9,93 @@ import ModelManagement
 verification : Model -> Model
 verification model =
     filterLinkNodeWithAscendant_ model
+
+
+verificationBlocs : Model -> Model
+verificationBlocs model =
+    verifUnicityIdBloc_ model
+
+
+
+-- verification blocs. existe t-il des blocs avec même id ?
+-- si oui, on le signale en log
+-- on cherche le max id du model : getCurIdFromModel
+-- pour i = 0 a maxId, trouver la liste blocs(id=i). La taille de la liste doit etre < 2
+
+
+unicityBlocId_ : Identifier -> Model -> Model
+unicityBlocId_ id model =
+    let
+        list =
+            List.filter (\x -> (x.id == id)) model.nodes
+
+        msg =
+            case (List.length list) > 1 of
+                True ->
+                    "Error doublons pour id " ++ (toString id)
+
+                False ->
+                    ""
+
+        z =
+            case String.isEmpty msg of
+                True ->
+                    ""
+
+                False ->
+                    Debug.log "unicityBlocId_ " msg
+    in
+        model
+
+
+unicityBlocListId_ : List Identifier -> Model -> Model
+unicityBlocListId_ list model =
+    case list of
+        [] ->
+            model
+
+        x :: xs ->
+            unicityBlocListId_ xs (unicityBlocId_ x model)
+
+
+mk_ : Identifier -> Identifier -> List Identifier -> List Identifier
+mk_ maxId i l =
+    case i > maxId of
+        True ->
+            l
+
+        False ->
+            mk_ maxId (i + 1) (i :: l)
+
+
+mkListId : Identifier -> List Identifier
+mkListId maxId =
+    let
+        list =
+            mk_ maxId 0 []
+
+        z =
+            Debug.log "mkListId " ( maxId, list )
+    in
+        list
+
+
+verifUnicityIdBloc_ : Model -> Model
+verifUnicityIdBloc_ model =
+    let
+        maxId =
+            DataModel.getCurIdFromModel model
+
+        z =
+            Debug.log "getCurIdFromModel" maxId
+
+        list =
+            mkListId maxId
+
+        m1 =
+            unicityBlocListId_ list model
+    in
+        m1
 
 
 
