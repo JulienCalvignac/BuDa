49bd2cada5cfd1d1665276e4dcd218201eb327aa
PICARD Michel <michel.picard@sirehna.com>
2018-01-25 16:12:08 +0100

    ajout implementation mise a jour de la liste des notifications reçues

--- graphTool/gui/src/NotificationActions.elm
+++ graphTool/gui/src/NotificationActions.elm
@@ -1,9 +1,31 @@
-module NotificationActions exposing (notificationData)
+module NotificationActions exposing (notificationData, updateNotificationModel)
 
-import Notifications exposing (Model, NotificationData, notifyData)
+import Notification exposing (Model, NotificationData, notifyData)
 import DataModelEncoders exposing (encodeNotification)
+import DataModelDecoders
+import Json.Decode
 
 
 notificationData : String -> NotificationData -> String
 notificationData s nd =
     encodeNotification (notifyData s nd)
+
+
+updateNotificationModel : String -> List NotificationData -> List NotificationData
+updateNotificationModel s list =
+    let
+        res_elts =
+            Json.Decode.decodeString DataModelDecoders.decodeNotification s
+
+        z =
+            Debug.log "updateNotificationModel" res_elts
+
+        newList =
+            case res_elts of
+                Ok element ->
+                    element.data :: list
+
+                Err _ ->
+                    list
+    in
+        newList
