ea8cd853bceefd0b6a6d13b2893c275ba46dcfdb
PICARD Michel <michel.picard@sirehna.com>
2018-01-25 15:54:01 +0100

    renommage du fichier Notification

---
+++ graphTool/gui/src/Notification.elm
@@ -0,0 +1,27 @@
+module Notification
+    exposing
+        ( Model
+        , NotificationData(..)
+        , notifyData
+        )
+
+import Node exposing (Node)
+import Link exposing (Edge)
+
+
+type NotificationData
+    = BLOC Node
+    | LIEN Edge
+
+
+type alias Model =
+    { header : String
+    , data : NotificationData
+    }
+
+
+notifyData : String -> NotificationData -> Model
+notifyData s notifyData =
+    { header = s
+    , data = notifyData
+    }
