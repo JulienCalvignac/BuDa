86507264a1f111cfb4eec6b0fb283727a333ee7b
PICARD Michel <michel.picard@sirehna.com>
2018-02-05 10:18:51 +0100

    Ajout implementation showPBS,showGeometry,showBulles,showAllData,showAllDataLight

--- graphTool/gui/src/ModelViews.elm
+++ graphTool/gui/src/ModelViews.elm
@@ -6,6 +6,11 @@ module ModelViews
         , getPBSViewFromNodeId
         , getPBSViewFromNodeName
         , getGeometryView
+        , showAllDataLight
+        , showAllData
+        , showPBS
+        , showBulles
+        , showGeometry
         )
 
 import Identifier exposing (Identifier)
@@ -13,6 +18,9 @@ import DataModel
 import ModelManagement
 import Node
 import Geometries
+import Model
+import ModelActions
+import DataModelActions
 
 
 getPBSView : DataModel.Model -> DataModel.Model
@@ -98,3 +106,128 @@ getGeometryViewFromId_ model id =
                 model.edges
     in
         { model | nodes = newNodes, edges = newEdges }
+
+
+showPBS : Model.Model -> DataModel.Model
+showPBS model =
+    let
+        subModel =
+            case model.nodeViewId of
+                Nothing ->
+                    (getPBSView model.dataModel)
+
+                Just x ->
+                    (getPBSViewFromNodeId model.dataModel x)
+    in
+        subModel
+
+
+showAllDataLight : Model.Model -> Model.Model
+showAllDataLight model =
+    modelDataShowAllDataLight_ (updateNodesPositionForLayout_ model)
+
+
+showAllData : Model.Model -> Model.Model
+showAllData model =
+    modelShowAllData_ (updateNodesPositionForLayout_ model)
+
+
+showBulles : Model.Model -> Model.Model
+showBulles model =
+    modelShowBulles_ (updateNodesPositionForLayout_ model)
+
+
+showGeometry : Model.Model -> Model.Model
+showGeometry model =
+    modelShowGeometry_ (updateNodesPositionForLayout_ model)
+
+
+modelShowGeometry_ : Model.Model -> Model.Model
+modelShowGeometry_ model =
+    let
+        dm =
+            (getGeometryView model.dataModel model.geometryId)
+
+        dm2 =
+            (DataModel.triNodes dm)
+    in
+        { model | dataModel = dm2 }
+
+
+modelShowBulles_ : Model.Model -> Model.Model
+modelShowBulles_ model =
+    let
+        m1 =
+            ModelManagement.filterWithMask model.dataModel
+
+        subModel =
+            case model.nodeViewId of
+                Nothing ->
+                    (getBullesView m1)
+
+                Just x ->
+                    (getBullesViewFromNodeId m1 x)
+
+        m2 =
+            (DataModel.triNodes subModel)
+    in
+        { model | dataModel = m2 }
+
+
+modelShowAllData_ : Model.Model -> Model.Model
+modelShowAllData_ model =
+    let
+        subModel =
+            model.dataModel
+
+        m2 =
+            case subModel.mustLayout of
+                True ->
+                    subModel
+
+                False ->
+                    DataModel.triNodes subModel
+    in
+        { model | dataModel = m2 }
+
+
+modelDataShowAllDataLight_ : Model.Model -> Model.Model
+modelDataShowAllDataLight_ model =
+    let
+        subModel =
+            model.dataModel
+
+        -- on garde les liens de plus bas niveau
+        lowestEdges =
+            DataModelActions.lowestLevelEdges model.dataModel
+
+        m2 =
+            { subModel | edges = lowestEdges }
+
+        m3 =
+            case subModel.mustLayout of
+                True ->
+                    m2
+
+                False ->
+                    DataModel.triNodes m2
+    in
+        { model | dataModel = m3 }
+
+
+updateNodesPositionForLayout_ : Model.Model -> Model.Model
+updateNodesPositionForLayout_ model =
+    let
+        dataModel =
+            model.dataModel
+
+        newDatamodel =
+            { dataModel | mustLayout = True }
+
+        m0 =
+            { model | dataModel = newDatamodel }
+
+        m1 =
+            (ModelActions.updateNodesPosition m0)
+    in
+        m1
