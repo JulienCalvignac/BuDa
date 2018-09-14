637899b84b9969c215f2423170ea880a4ec6a9aa
Bryan Colle <bryan.colle@sirehna.com>
2018-09-14 16:28:51 +0200

    init and subscriptions moved to Main instead of Simple

--- graphTool/gui/src/Main.elm
+++ graphTool/gui/src/Main.elm
@@ -3,32 +3,41 @@ module Main exposing (..)
 import Html exposing (Html)
 import View
 import Actions
-import Simple
 import Messages exposing (Msg(..))
-import Model exposing (Model)
+import Model exposing (Model, defaultModel)
+import LinkToJS
+import Keyboard
 
 
 main : Program Never Model Msg
 main =
     Html.program
-        { init = Simple.init
+        { init = init
         , view = View.view
         , update = Actions.update
-        , subscriptions = Simple.subscriptions
+        , subscriptions = subscriptions
         }
 
 
+init : ( Model, Cmd Msg )
+init =
+    ( defaultModel, Cmd.none )
 
--- import Html exposing (Html)
--- import ModelWithUndo exposing (Model, Msg(..))
--- import SimpleWithUndo
---
---
--- main : Program Never Model Msg
--- main =
---     Html.program
---         { init = SimpleWithUndo.init
---         , view = SimpleWithUndo.view
---         , update = SimpleWithUndo.update
---         , subscriptions = SimpleWithUndo.subscriptions
---         }
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    -- Sub.none
+    Sub.batch
+        [ LinkToJS.selection Selection
+        , LinkToJS.modeltoelm ModelToElm
+        , LinkToJS.csvmodeltoelm CsvModelToElm
+        , LinkToJS.csv2modeltoelm Csv2ModelToElm
+        , LinkToJS.importModeltoelm ImportModelToElm
+        , LinkToJS.importCsvModeltoelm ImportCsvModeltoElm
+        , LinkToJS.doubleclick DoubleClick
+        , LinkToJS.nodesPositionToElm NodesPositionToElm
+        , LinkToJS.nodesPositionRequest NodesPositionRequest
+        , LinkToJS.sendGeometryToElm SendGeometryToElm
+        , Keyboard.ups KeyUps
+        , Keyboard.downs KeyDowns
+        ]
